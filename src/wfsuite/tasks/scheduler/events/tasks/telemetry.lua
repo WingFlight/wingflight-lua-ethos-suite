b92af64edd84ab8a9194575183cc5a8f5dae863a
Rob Thomson <rob@marotori.com>
2026-07-05 17:21:13 +0100

    Create distinct manual vs passthrough mode.

--- src/wfsuite/tasks/scheduler/events/tasks/telemetry.lua
+++ src/wfsuite/tasks/scheduler/events/tasks/telemetry.lua
@@ -63,7 +63,8 @@ local armMap = {[0] = "disarmed.wav", [1] = "armed.wav", [2] = "disarmed.wav", [
 local FLIGHT_MODE_PRIORITY = {
     {bit = 0, file = "failsafe.wav"},     -- FAILSAFE_MODE_BIT
     {bit = 6, file = "gpsrescue.wav"},    -- GPS_RESCUE_MODE_BIT
-    {bit = 7, file = "manual.wav"},       -- PASSTHROUGH_MODE_BIT
+    {bit = 7, file = "passthrough.wav"},  -- PASSTHROUGH_MODE_BIT
+    {bit = 10, file = "manual.wav"},      -- MANUAL_MODE_BIT
     {bit = 9, file = "autohover.wav"},    -- AUTOHOVER_MODE_BIT
     {bit = 2, file = "horizon.wav"},      -- HORIZON_MODE_BIT
     {bit = 1, file = "angle.wav"},        -- ANGLE_MODE_BIT
