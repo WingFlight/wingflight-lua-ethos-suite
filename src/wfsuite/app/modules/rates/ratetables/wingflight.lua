0a94e6f7a81181761e55e757cf35d068750278c3
Rob Thomson <rob@marotori.com>
2026-07-07 21:34:42 +0100

    Adjust default rate curves

--- src/wfsuite/app/modules/rates/ratetables/wingflight.lua
+++ src/wfsuite/app/modules/rates/ratetables/wingflight.lua
@@ -13,17 +13,17 @@ local apidata = {
         rows = {"@i18n(app.modules.rates.roll)@", "@i18n(app.modules.rates.pitch)@", "@i18n(app.modules.rates.yaw)@"},
         cols = {"@i18n(app.modules.rates.rate)@", "@i18n(app.modules.rates.shape)@", "@i18n(app.modules.rates.expo)@"},
         fields = {
-            {row = 1, col = 1, min = 0, max = 100, default = 49, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_1"},
-            {row = 2, col = 1, min = 0, max = 100, default = 48, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_2"},
-            {row = 3, col = 1, min = 0, max = 200, default = 25, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_3"},
+            {row = 1, col = 1, min = 0, max = 100, default = 50, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_1"},
+            {row = 2, col = 1, min = 0, max = 100, default = 50, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_2"},
+            {row = 3, col = 1, min = 0, max = 200, default = 70, mult = 5, step = 5, mspapi = 1, apikey = "rcRates_3"},
 
-            {row = 1, col = 2, min = 0, max = 127, default = 12, mult = 1, step = 1, mspapi = 1, apikey = "rates_1"},
-            {row = 2, col = 2, min = 0, max = 127, default = 12, mult = 1, step = 1, mspapi = 1, apikey = "rates_2"},
-            {row = 3, col = 2, min = 0, max = 127, default = 12, mult = 1, step = 1, mspapi = 1, apikey = "rates_3"},
+            {row = 1, col = 2, min = 0, max = 127, default = 5, mult = 1, step = 1, mspapi = 1, apikey = "rates_1"},
+            {row = 2, col = 2, min = 0, max = 127, default = 5, mult = 1, step = 1, mspapi = 1, apikey = "rates_2"},
+            {row = 3, col = 2, min = 0, max = 127, default = 5, mult = 1, step = 1, mspapi = 1, apikey = "rates_3"},
 
-            {row = 1, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_1"},
-            {row = 2, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_2"},
-            {row = 3, col = 3, min = 0, max = 100, default = 0, mspapi = 1, apikey = "rcExpo_3"}
+            {row = 1, col = 3, min = 0, max = 100, default = 30, mspapi = 1, apikey = "rcExpo_1"},
+            {row = 2, col = 3, min = 0, max = 100, default = 30, mspapi = 1, apikey = "rcExpo_2"},
+            {row = 3, col = 3, min = 0, max = 100, default = 30, mspapi = 1, apikey = "rcExpo_3"}
         }
     }
 }
